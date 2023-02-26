c7363f3f32dfb687f172162e112edf81ab47a999
subAngel <whoangel.agl@gmail.com>
2023-02-26 10:22:57 -0600

    Facade: class check facade lista

--- src/facade/facade/CheckFacade.java
+++ src/facade/facade/CheckFacade.java
@@ -1,4 +1,19 @@
 package facade.facade;
 
+import facade.api.AvionAPI;
+import facade.api.HotelAPI;
+
 public class CheckFacade {
+    private AvionAPI avionAPI;
+    private HotelAPI hotelAPI;
+
+    public CheckFacade(){
+        avionAPI = new AvionAPI();
+        hotelAPI = new HotelAPI();
+    }
+
+    public void buscar(String fechaIda, String fechaVuelta, String origen, String destino){
+        avionAPI.buscarVuelos(fechaIda, fechaVuelta, origen, destino);
+        hotelAPI.buscarHoteles(fechaIda, fechaVuelta, origen, destino);
+    }
 }
