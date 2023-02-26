c60b3fd05e7618bb5ac2bfae0601e5412b1f1ba3
subAngel <whoangel.agl@gmail.com>
2023-02-26 10:19:32 -0600

    Facade: clases de api listas

--- src/facade/api/AvionAPI.java
+++ src/facade/api/AvionAPI.java
@@ -1,4 +1,10 @@
 package facade.api;
 
 public class AvionAPI {
+    public void buscarVuelos(String fechaIda, String fechaVuelta, String origen, String destino){
+        System.out.println("=================================");
+        System.out.printf("Vuelos encotrados para %s desde %s\n", destino, origen);
+        System.out.printf("Fecha Ida: %s\tFecha Vuelta: %s\n", fechaIda, fechaVuelta);
+        System.out.println("=================================");
+    }
 }
--- src/facade/api/HotelAPI.java
+++ src/facade/api/HotelAPI.java
@@ -1,4 +1,13 @@
 package facade.api;
 
 public class HotelAPI {
+    public void buscarHoteles(String fechaEntrada, String fechaSalida, String origen, String destino){
+        System.out.println(====================================================);
+        System.out.println("Hoteles encontrados");
+        System.out.printf("Entrada: %s\tSalida: %s", fechaEntrada, fechaSalida);
+        System.out.println("Hotel A");
+        System.out.println("Hotel B");
+        System.out.println("Hotel C");
+        System.out.println(====================================================);
+    }
 }
