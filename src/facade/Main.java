d745f9eda3d04e7c1021dbc057ba6372914a56f7
subAngel <whoangel.agl@gmail.com>
2023-02-26 10:28:56 -0600

    Facade: ejemplo mitocode

--- src/facade/Main.java
+++ src/facade/Main.java
@@ -1,4 +1,17 @@
 package facade;
 
+import facade.facade.CheckFacade;
+
 public class Main {
+    public static void main(String[] args) {
+        CheckFacade cliente1 = new CheckFacade();
+        cliente1.buscar("02/07/2023", "08/07/2023", "Mexico", "Oaxaca");
+
+        CheckFacade cliente2 = new CheckFacade();
+        cliente1.buscar("03/11/2023", "06/11/2023", "Mexico", "Oaxaca");
+
+    }
+
+
+
 }
--- src/facade/api/HotelAPI.java
+++ src/facade/api/HotelAPI.java
@@ -2,12 +2,12 @@ package facade.api;
 
 public class HotelAPI {
     public void buscarHoteles(String fechaEntrada, String fechaSalida, String origen, String destino){
-        System.out.println(====================================================);
+        System.out.println("====================================================");
         System.out.println("Hoteles encontrados");
         System.out.printf("Entrada: %s\tSalida: %s", fechaEntrada, fechaSalida);
         System.out.println("Hotel A");
         System.out.println("Hotel B");
         System.out.println("Hotel C");
-        System.out.println(====================================================);
+        System.out.println("====================================================");
     }
 }
